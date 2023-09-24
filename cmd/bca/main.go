37fd30411f33a8435ceb5932500e8384954039c2
Andres Court <andres@andrescourt.com>
2023-09-23 23:38:08 -0500

    Starting the server on the PORT defined

--- cmd/bca/main.go
+++ cmd/bca/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Home endpoint hit")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Hello World"))
 	})
-	log.Panic(http.ListenAndServe(":42069", nil))
+	log.Println(":INFO: Server listening in port: ", port)
+	log.Panic(http.ListenAndServe(":"+port, nil))
 }
